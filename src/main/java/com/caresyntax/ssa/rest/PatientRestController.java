0ed51f05d938318d6e15abc07a2374edf91fe34d
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-28 09:25:17 +0530

    patient rest controller for all patient rest services

---
+++ src/main/java/com/caresyntax/ssa/rest/PatientRestController.java
@@ -0,0 +1,73 @@
+package com.caresyntax.ssa.rest;
+
+import com.caresyntax.ssa.dto.PatientDto;
+import com.caresyntax.ssa.dto.SsaSimpleResponse;
+import com.caresyntax.ssa.exception.SsaInvalidDataException;
+import com.caresyntax.ssa.model.Gender;
+import com.caresyntax.ssa.model.Patient;
+import com.caresyntax.ssa.service.IPatientService;
+import com.caresyntax.ssa.utility.IConstants;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("http://localhost:4200/")
+@RestController
+@RequestMapping("/api/v1/patient")
+@Log4j2
+public class PatientRestController {
+
+    IPatientService patientService;
+
+    @Autowired
+    public void setPatientService(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getAllPatients() {
+        log.info("START PatientRestController getAllPatients");
+
+        Optional optionalPatientList;
+        SsaSimpleResponse getPatientsResponse = new SsaSimpleResponse();
+        getPatientsResponse.setMessage(IConstants.NO_PATIENTS);
+        getPatientsResponse.setData(Collections.EMPTY_LIST);
+
+        try {
+            optionalPatientList = this.patientService.getAllPatients();
+
+            if (optionalPatientList.isPresent()) {
+                List<Patient> patientList = (List<Patient>) optionalPatientList.get();
+                getPatientsResponse.setData(patientList);
+            }
+        } catch (Exception ex) {
+            log.warn("Unable to get patient data");
+        }
+
+        log.info("END PatientRestController getAllPatients");
+        return ResponseEntity.ok(getPatientsResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity addPatient(@RequestBody PatientDto patientData) throws SsaInvalidDataException {
+        log.info("START PatientRestController addPatient");
+        SsaSimpleResponse addPatientResponse = new SsaSimpleResponse();
+        addPatientResponse.setMessage(IConstants.UNABLE_ADD_PATIENT);
+
+        final Patient patient = this.patientService.createPatientModel(patientData);
+        final Patient sPatient = (Patient) this.patientService.addPatient(patient);
+
+        if (sPatient.getId() != null) {
+            log.info("Patient saved successfully");
+            addPatientResponse.setMessage(IConstants.PATIENT_ADDED);
+        }
+        log.info("END PatientRestController addPatient");
+        return ResponseEntity.ok(addPatientResponse);
+    }
+}
