9382497d16fa302ca11ac0e389f8d496d9ad3212
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 00:47:02 +0530

    added routes for new components

--- ssa-ng-app/src/app/routes.ts
+++ ssa-ng-app/src/app/routes.ts
@@ -1,11 +1,26 @@
 import { Route } from '@angular/router';
-import { AddPatientComponent } from './add-patient/add-patient.component';
+import { AddPatientComponent } from './routes/add-patient/add-patient.component';
 import { AppComponent } from './app.component';
+import { StudySchedulerComponent } from './routes/study-scheduler/study-scheduler.component';
+import { RoomListingComponent } from './routes/room-listing/room-listing.component';
+import { DoctorListingComponent } from './routes/doctor-listing/doctor-listing.component';
 
 export const route: Route[] = [
   {
     path: 'addPatient',
     component: AddPatientComponent
+  },
+  {
+    path: 'schedule',
+    component: StudySchedulerComponent
+  },
+  {
+    path: 'rl',
+    component: RoomListingComponent
+  },
+  {
+    path: 'dc',
+    component: DoctorListingComponent
   },
   {
     path: '*',
