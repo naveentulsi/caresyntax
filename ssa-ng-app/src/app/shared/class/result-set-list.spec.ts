f31e4bbc33a1acf6c8b2961a654098e1aef40a63
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 00:56:18 +0530

    common result set list

---
+++ ssa-ng-app/src/app/shared/class/result-set-list.spec.ts
@@ -0,0 +1,7 @@
+import { ResultSetList } from './result-set-list';
+
+describe('ResultSetList', () => {
+  it('should create an instance', () => {
+    expect(new ResultSetList()).toBeTruthy();
+  });
+});
---
+++ ssa-ng-app/src/app/shared/class/result-set-list.ts
@@ -0,0 +1,5 @@
+export class ResultSetList {
+    id: number;
+    name: string;
+}
+
