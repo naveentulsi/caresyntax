b009cf1fdcb26b38980bec0323e1dd57e38a08fd
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-28 09:23:44 +0530

    interface for patient services

---
+++ src/main/java/com/caresyntax/ssa/service/IPatientService.java
@@ -0,0 +1,15 @@
+package com.caresyntax.ssa.service;
+
+import com.caresyntax.ssa.dto.PatientDto;
+import com.caresyntax.ssa.model.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPatientService<T> {
+    Optional<List<T>> getAllPatients();
+
+    T addPatient(T patient);
+
+    Patient createPatientModel(PatientDto patientData);
+}
