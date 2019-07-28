251edd99e8a1bec305b6200029e5f73c47a59d14
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 00:48:30 +0530

    added new components, service & momentjs

--- ssa-ng-app/src/app/app.module.ts
+++ ssa-ng-app/src/app/app.module.ts
@@ -3,26 +3,52 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddPatientComponent } from './add-patient/add-patient.component';
+import { AddPatientComponent } from './routes/add-patient/add-patient.component';
 import { RouterModule } from '@angular/router';
 import { route } from './routes';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { PatientService } from './shared/services/patient/patient.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudySchedulerComponent } from './routes/study-scheduler/study-scheduler.component';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
+import { DyanmicListingComponent } from './shared/components/dyanmic-listing/dyanmic-listing.component';
+import { RoomService } from './shared/services/room/room.service';
+import { DoctorService } from './shared/services/doctor/doctor.service';
+import { RoomListingComponent } from './routes/room-listing/room-listing.component';
+import { DoctorListingComponent } from './routes/doctor-listing/doctor-listing.component';
+
+export const MY_MOMENT_FORMATS = {
+  datePickerInput: 'l',
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddPatientComponent
+    AddPatientComponent,
+    StudySchedulerComponent,
+    DyanmicListingComponent,
+    RoomListingComponent,
+    DoctorListingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(route, {useHash: true})
+    FormsModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      maxOpened: 3
+    }),
+    RouterModule.forRoot(route, { useHash: true })
   ],
-  providers: [PatientService, HttpClientModule],
+  providers: [PatientService, RoomService, DoctorService, HttpClientModule, {provide: OWL_DATE_TIME_FORMATS, useValue: MY_MOMENT_FORMATS}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
