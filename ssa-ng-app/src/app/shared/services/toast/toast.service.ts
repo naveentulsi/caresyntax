7a58027e8c36df2ddc436ab1a75e8ee43dfdb5fc
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 00:50:23 +0530

    common toastr service

---
+++ ssa-ng-app/src/app/shared/services/toast/toast.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(private toastr: ToastrService) { }
+
+  showsSuccess(message: string) {
+    this.toastr.success(message, 'Success');
+  }
+
+  showError(errorMessage: string) {
+    this.toastr.error(errorMessage, 'Error');
+  }
+}
