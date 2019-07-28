6a0a4b0e870a10118409a1be54713719601c1bf2
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-28 09:34:51 +0530

    added utility

---
+++ src/main/java/com/caresyntax/ssa/utility/IConstants.java
@@ -0,0 +1,8 @@
+package com.caresyntax.ssa.utility;
+
+public interface IConstants {
+    String NO_PATIENTS = "No patient data";
+    String UNABLE_ADD_PATIENT = "Server unbale to add patient";
+    String PATIENT_ADDED = "Patient added successfully";
+    String DEFAULT_SCHEMA = "caresyntax";
+}
