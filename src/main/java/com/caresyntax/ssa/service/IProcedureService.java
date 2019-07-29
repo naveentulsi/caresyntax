851c5633f48709f1785029370df38914cccaa535
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 20:57:52 +0530

    added method to find procedure by id.

--- src/main/java/com/caresyntax/ssa/service/IProcedureService.java
+++ src/main/java/com/caresyntax/ssa/service/IProcedureService.java
@@ -5,9 +5,12 @@ import com.caresyntax.ssa.exception.SsaInvalidDataException;
 import com.caresyntax.ssa.model.Procedure;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProcedureService<T> {
     Procedure saveProcedure(T procedure);
+
+    Optional<Procedure> findById(Integer Id);
 
     List<T> getAllProcedure();
 
