5bee7c94f19c7b39a72581aee6bc6c2fdee21065
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 19:55:24 +0530

    added more messages

--- src/main/java/com/caresyntax/ssa/utility/IConstants.java
+++ src/main/java/com/caresyntax/ssa/utility/IConstants.java
@@ -1,8 +1,12 @@
 package com.caresyntax.ssa.utility;
 
 public interface IConstants {
-    String NO_PATIENTS = "No patient data";
-    String UNABLE_ADD_PATIENT = "Server unbale to add patient";
+    String NO_PATIENT_FETCHED = "No patient data";
+    String NO_DOCTOR_FETCHED = "No doctors has been fetched";
+    String DOCTOR_FETCHED = "Successfully fetched doctors";
+    String PATIENT_FETCHED = "Successfully fetched patients";
+    String UNABLE_ADD_PATIENT = "Server unable to add patient";
     String PATIENT_ADDED = "Patient added successfully";
     String DEFAULT_SCHEMA = "caresyntax";
+    String DATE_FORMAT = "yyyy/MM/dd";
 }
