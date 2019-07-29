e46c31ea8b902479944c6a75baa0f5bb7e498c93
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 19:55:50 +0530

    added method to save doctor

--- src/main/java/com/caresyntax/ssa/service/IDoctorService.java
+++ src/main/java/com/caresyntax/ssa/service/IDoctorService.java
@@ -1,8 +1,17 @@
 package com.caresyntax.ssa.service;
 
 
+import com.caresyntax.ssa.model.Doctor;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 public interface IDoctorService<T> {
     List<T> getAllDoctors();
+
+    Optional<Doctor> getDoctorById(Integer Id);
+
+    @Transactional
+    Doctor saveDoctor(Doctor doctor);
 }
