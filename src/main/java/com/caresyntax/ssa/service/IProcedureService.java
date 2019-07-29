e653e08124a8a2a22a2314ceb226cddb11619c8b
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 19:58:30 +0530

    added method for procedure CRUD

--- src/main/java/com/caresyntax/ssa/service/IProcedureService.java
+++ src/main/java/com/caresyntax/ssa/service/IProcedureService.java
@@ -1,4 +1,15 @@
 package com.caresyntax.ssa.service;
 
+import com.caresyntax.ssa.dto.ProcedureDto;
+import com.caresyntax.ssa.exception.SsaInvalidDataException;
+import com.caresyntax.ssa.model.Procedure;
+
+import java.util.List;
+
 public interface IProcedureService<T> {
+    Procedure saveProcedure(T procedure);
+
+    List<T> getAllProcedure();
+
+    T validateAndSetProcedureData(ProcedureDto procedureDto) throws SsaInvalidDataException;
 }
