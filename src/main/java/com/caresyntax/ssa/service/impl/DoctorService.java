429c7196d50da3c1e8e4b34792f284fe59afd03a
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 19:55:02 +0530

    added method comments and logger

--- src/main/java/com/caresyntax/ssa/service/impl/DoctorService.java
+++ src/main/java/com/caresyntax/ssa/service/impl/DoctorService.java
@@ -7,7 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Log4j2
@@ -24,5 +26,30 @@ public class DoctorService implements IDoctorService<Doctor> {
     public List<Doctor> getAllDoctors() {
         log.info("START DoctorService getAllDoctors");
         return this.doctorRepository.findAll();
+    }
+
+    /**
+     * Get doctor by Id
+     *
+     * @param Id doctor id
+     * @return doctor wrapped in a optional
+     */
+    @Override
+    public Optional<Doctor> getDoctorById(Integer Id) {
+        log.info("START DoctorService getDoctorById");
+        return this.doctorRepository.findById(Id);
+    }
+
+    /**
+     * Persists doctor entity
+     *
+     * @param doctor doctor instance to be persisted
+     * @return persisted instance
+     */
+    @Transactional
+    @Override
+    public Doctor saveDoctor(Doctor doctor) {
+        log.info("START DoctorService saveDoctor");
+        return this.doctorRepository.save(doctor);
     }
 }
