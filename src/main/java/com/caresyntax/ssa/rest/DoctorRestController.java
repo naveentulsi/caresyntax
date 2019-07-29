8c15b67c86bd87a443054880a96e104dbfd3eefb
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 19:54:31 +0530

    added post method and minor fixes

--- src/main/java/com/caresyntax/ssa/rest/DoctorRestController.java
+++ src/main/java/com/caresyntax/ssa/rest/DoctorRestController.java
@@ -4,13 +4,12 @@ package com.caresyntax.ssa.rest;
 import com.caresyntax.ssa.dto.SsaSimpleResponse;
 import com.caresyntax.ssa.model.Doctor;
 import com.caresyntax.ssa.service.IDoctorService;
+import com.caresyntax.ssa.utility.IConstants;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,15 +30,33 @@ public class DoctorRestController {
     public SsaSimpleResponse getAllDoctor() {
         log.info("START DoctorRestController getAllDoctor");
         SsaSimpleResponse ssaSimpleResponse = new SsaSimpleResponse();
-        ssaSimpleResponse.setMessage("No doctors has been fetched");
+        ssaSimpleResponse.setMessage(IConstants.NO_DOCTOR_FETCHED);
 
         final List<Doctor> doctorList = this.doctorService.getAllDoctors();
         if (doctorList != null && doctorList.size() > 0) {
             log.info(String.format("Total doctors fetched %d", doctorList.size()));
             ssaSimpleResponse.setData(doctorList);
+            ssaSimpleResponse.setMessage(IConstants.DOCTOR_FETCHED);
         }
 
         log.info("END DoctorRestController getAllDoctor");
         return ssaSimpleResponse;
     }
+
+    @PostMapping(name = "Store doctor data to system", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SsaSimpleResponse> addDoctor(@RequestBody Doctor doctor) {
+        log.info("START DoctorRestController addDoctor");
+        SsaSimpleResponse ssaSimpleResponse = new SsaSimpleResponse();
+        ssaSimpleResponse.setMessage("Unable to save doctor now.");
+
+        final Doctor sDoctor = this.doctorService.saveDoctor(doctor);
+        if (sDoctor.getId() != null) {
+            ssaSimpleResponse.setMessage("Doctor saved.");
+            ssaSimpleResponse.setData(sDoctor);
+        }
+
+        log.info("END DoctorRestController addDoctor");
+        return ResponseEntity.ok(ssaSimpleResponse);
+    }
+
 }
