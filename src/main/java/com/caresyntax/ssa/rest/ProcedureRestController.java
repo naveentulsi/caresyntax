a90573aa173a288d372a6ab22c203102dbd3b787
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 20:58:19 +0530

    added put method to update procedure status

--- src/main/java/com/caresyntax/ssa/rest/ProcedureRestController.java
+++ src/main/java/com/caresyntax/ssa/rest/ProcedureRestController.java
@@ -5,6 +5,7 @@ import com.caresyntax.ssa.dto.SsaSimpleResponse;
 import com.caresyntax.ssa.model.Procedure;
 import com.caresyntax.ssa.service.IProcedureService;
 import com.caresyntax.ssa.service.impl.ProcedureService;
+import com.caresyntax.ssa.utility.IConstants;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+//only for local development
+//TODO - remove before deployment.
 @CrossOrigin("http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/proc")
@@ -59,6 +63,39 @@ public class ProcedureRestController {
 
         log.info("END ProcedureRestController getAllProcedure");
         return new ResponseEntity<>(ssaSimpleResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, name = "Update procedure status")
+    public ResponseEntity<SsaSimpleResponse> updateProcedure(@RequestBody Procedure procedure) {
+        log.info("START ProcedureRestController updateProcedure");
+        SsaSimpleResponse ssaSimpleResponse = new SsaSimpleResponse();
+        ssaSimpleResponse.setMessage("Server unable to update procedures");
+
+        if (procedure.getId() != null) {
+            final Optional<Procedure> procedureOptional = this.procedureService.findById(procedure.getId());
+
+            if (procedureOptional.isPresent()) {
+                final Procedure sProcedure = procedureOptional.get();
+                log.info("Procedure instance fetched");
+
+                if (procedure.getStatus() != null) {
+                    sProcedure.setStatus(procedure.getStatus());
+                    this.procedureService.saveProcedure(sProcedure);
+                    ssaSimpleResponse.setMessage(IConstants.PROCEDURE_UPDATED);
+                } else {
+                    log.info("No status field");
+                    ssaSimpleResponse.setMessage(IConstants.PROCEDURE_EMPTY_STATUS);
+                }
+
+            } else {
+                ssaSimpleResponse.setMessage(IConstants.PROCEDURE_INVALID_ID);
+            }
+
+        } else {
+            ssaSimpleResponse.setMessage(IConstants.PROCEDURE_NO_ID);
+        }
+        log.info("END ProcedureRestController updateProcedure");
+        return ResponseEntity.ok(ssaSimpleResponse);
     }
 
 }
