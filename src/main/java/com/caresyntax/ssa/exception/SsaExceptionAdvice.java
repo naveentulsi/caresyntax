b472dea1f89891fbd9253b499c9d187dcb65b95c
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 20:12:20 +0530

    added method comments

--- src/main/java/com/caresyntax/ssa/exception/SsaExceptionAdvice.java
+++ src/main/java/com/caresyntax/ssa/exception/SsaExceptionAdvice.java
@@ -9,26 +9,50 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Log4j2
 public class SsaExceptionAdvice {
 
+    /**
+     * Catches all exception arising out of app, except those listed below.
+     *
+     * @param ex - Exception instance
+     * @return Safe response - saying server is down.
+     */
     @ExceptionHandler({Throwable.class, Exception.class})
-    public ResponseEntity HandleAll(Exception ex){
+    public ResponseEntity HandleAll(Exception ex) {
         log.info("Exception Caught");
-        return (ex == null) ?  error() : error(ex, ex.getMessage());
+        return (ex == null) ? error() : error(ex, ex.getMessage());
     }
 
+    /**
+     * To handle all invalid data exception out of app, reason and message is contained in exception instance. Same data is transported to API user as response body.
+     *
+     * @param ex - SsaInvalidDataException instance
+     * @return Safe response - stating actual reason.
+     */
     @ExceptionHandler(SsaInvalidDataException.class)
-    public ResponseEntity HandleInvalidDataException(SsaInvalidDataException ex){
+    public ResponseEntity HandleInvalidDataException(SsaInvalidDataException ex) {
         log.info("Exception Caught");
-        return (ex == null) ?  error() : error(ex, ex.getMessage());
+        return (ex == null) ? error() : error(ex, ex.getMessage());
     }
 
-    private ResponseEntity error(){
+    /**
+     * Creates the generic response object for handled exception calls, if the exception object is null
+     *
+     * @return
+     */
+    private ResponseEntity error() {
         return ResponseEntity.ok(new SsaExceptionResponse("Server Exception", "Server is down, try again in some time."));
     }
 
-    private ResponseEntity error(Exception ex, String message){
+    /**
+     * Method logs the exception and creates a generic response object with the reason & message to be sent to API user.
+     *
+     * @param ex
+     * @param message
+     * @return
+     */
+    private ResponseEntity error(Exception ex, String message) {
         String reason = ex.getClass().getName();
         logError(ex);
-        if(ex instanceof SsaInvalidDataException){
+        if (ex instanceof SsaInvalidDataException) {
             SsaInvalidDataException ssaInvalidDataException = (SsaInvalidDataException) ex;
             reason = ssaInvalidDataException.getReason();
             message = ssaInvalidDataException.getMessage();
@@ -37,7 +61,12 @@ public class SsaExceptionAdvice {
         return ResponseEntity.ok(new SsaExceptionResponse(reason, message));
     }
 
-    private void logError(Exception ex){
+    /**
+     * Plain exception logger for this class.
+     *
+     * @param ex exception instance
+     */
+    private void logError(Exception ex) {
         log.error(ex.getMessage(), ex);
     }
 
