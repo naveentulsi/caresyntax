36158c769918edcff1c831fd21139692514f086d
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 20:02:16 +0530

    added date type for hibernate, created date field and date formatter

--- src/main/java/com/caresyntax/ssa/model/Procedure.java
+++ src/main/java/com/caresyntax/ssa/model/Procedure.java
@@ -1,11 +1,14 @@
 package com.caresyntax.ssa.model;
 
+import com.caresyntax.ssa.utility.IConstants;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
@@ -13,18 +16,18 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Procedure {
+public class Procedure implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false)
     private Integer Id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "PATIENT_FK", nullable = false)
     private Patient patient;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "DOCTOR_FK", nullable = false)
     private Doctor doctor;
 
@@ -33,11 +36,27 @@ public class Procedure {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "STATUS", nullable = false)
-    private Procedure status;
+    private ProcedureStatus status;
 
     @Column(name = "PLANNED_START_TIME", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date plannedStartTime;
 
     @Column(name = "PLANNED_END_TIME")
+    @Temporal(TemporalType.DATE)
     private Date plannedEndTime;
+
+    @Column(name = "CREATED_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date createdDate;
+
+    @DateTimeFormat(pattern = IConstants.DATE_FORMAT)
+    public Date getPlannedStartTime() {
+        return plannedStartTime;
+    }
+
+    @DateTimeFormat(pattern = IConstants.DATE_FORMAT)
+    public Date getPlannedEndTime() {
+        return plannedEndTime;
+    }
 }
