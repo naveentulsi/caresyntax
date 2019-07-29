6eaca4f9ee01cb314b33b8da7e969247ff39b553
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 20:10:55 +0530

    added method to store procedure data.

--- src/main/java/com/caresyntax/ssa/rest/ProcedureRestController.java
+++ src/main/java/com/caresyntax/ssa/rest/ProcedureRestController.java
@@ -1,15 +1,16 @@
 package com.caresyntax.ssa.rest;
 
+import com.caresyntax.ssa.dto.ProcedureDto;
 import com.caresyntax.ssa.dto.SsaSimpleResponse;
 import com.caresyntax.ssa.model.Procedure;
+import com.caresyntax.ssa.service.IProcedureService;
 import com.caresyntax.ssa.service.impl.ProcedureService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +20,14 @@ import java.util.List;
 @Log4j2
 public class ProcedureRestController {
 
-    ProcedureService procedureService;
+    IProcedureService procedureService;
 
     @Autowired
     public void setProcedureService(ProcedureService procedureService) {
         this.procedureService = procedureService;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, name = "Get all procedures." )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, name = "Get all procedures.")
     public SsaSimpleResponse getAllProcedure() {
         log.info("START ProcedureRestController getAllProcedure");
         final List<Procedure> allProcedure = this.procedureService.getAllProcedure();
@@ -42,4 +43,22 @@ public class ProcedureRestController {
         log.info("END ProcedureRestController getAllProcedure");
         return ssaSimpleResponse;
     }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, name = "Save new procedure data")
+    public ResponseEntity<SsaSimpleResponse> storeProcedure(@RequestBody ProcedureDto procedureDto) {
+        log.info("START ProcedureRestController getAllProcedure");
+        SsaSimpleResponse ssaSimpleResponse = new SsaSimpleResponse();
+        ssaSimpleResponse.setMessage("Server unable to store procedures");
+
+        final Procedure procedure = this.procedureService.validateAndSetProcedureData(procedureDto);
+        final Procedure sProcedure = this.procedureService.saveProcedure(procedure);
+
+        if (sProcedure.getId() != null) {
+            ssaSimpleResponse.setMessage("Procedure has been saved successfully");
+        }
+
+        log.info("END ProcedureRestController getAllProcedure");
+        return new ResponseEntity<>(ssaSimpleResponse, HttpStatus.CREATED);
+    }
+
 }
