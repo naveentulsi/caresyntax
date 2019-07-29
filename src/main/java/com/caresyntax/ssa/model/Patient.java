3a8811cde4702ec6fc2bf0b848f895c020868105
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-29 09:38:51 +0530

    added no arg constructor& made id field unmodifiable.

--- src/main/java/com/caresyntax/ssa/model/Patient.java
+++ src/main/java/com/caresyntax/ssa/model/Patient.java
@@ -1,18 +1,22 @@
 package com.caresyntax.ssa.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name="PATIENT")
 @Getter
 @Setter
-public class Patient {
+@NoArgsConstructor
+public class Patient implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
     private Long Id;
 
     @Column(name = "NAME")
