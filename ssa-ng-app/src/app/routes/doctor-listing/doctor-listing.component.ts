cf9fb2da4c01a8298261bdb803d8135fff0de17d
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-31 03:16:52 +0530

    handles response and error

--- ssa-ng-app/src/app/routes/doctor-listing/doctor-listing.component.ts
+++ ssa-ng-app/src/app/routes/doctor-listing/doctor-listing.component.ts
@@ -8,28 +8,32 @@ import { DoctorService } from 'src/app/shared/services/doctor/doctor.service';
   styleUrls: ['./doctor-listing.component.css']
 })
 export class DoctorListingComponent implements OnInit {
-  doctorList: any;
+  doctorList: any[];
+  responseMessage: string;
   constructor(private doctorService: DoctorService, private toastService: ToastService) { }
 
   ngOnInit() {
     const doctor$ = this.doctorService.getAllDoctors();
     doctor$.subscribe((res: any) => {
       try {
-        this.doctorList = res.message;
+        // check data obect in response
+        if ('data' in res) {
+          this.doctorList = res.data;
+        }
+        // get response message
+        if ('message' in res) {
+          this.responseMessage = res.message;
+        }
+        // check errro obect in response
+        if ('error' in res) {
+          this.toastService.showError(this.responseMessage);
+        }
       } catch (err) {
-        this.showError(err);
+        this.toastService.showError('Unable to fetch patients.');
       }
     }, (err) => {
-      this.showError(err);
+      this.toastService.showError('Unable to fetch doctors.');
     }, () => {
     });
-  }
-  /**
-     * Shoots error toast message
-     * @param err error object to logged
-     */
-  private showError(err: any) {
-    this.toastService.showError('Not able to fetch dcotors.');
-    console.log(err);
   }
 }
