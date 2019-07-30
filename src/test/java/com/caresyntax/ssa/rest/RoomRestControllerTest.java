3030ab510ba070b88c1a189352aeec7a9063c32e
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-31 03:28:27 +0530

    Added unit test for Room Rest Controller

--- src/test/java/com/caresyntax/ssa/rest/RoomRestControllerTest.java
+++ src/test/java/com/caresyntax/ssa/rest/RoomRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.caresyntax.ssa.rest;
 
 import com.caresyntax.ssa.dto.SsaSimpleResponse;
+import com.caresyntax.ssa.model.Room;
 import com.caresyntax.ssa.service.IRoomService;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
@@ -10,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -26,11 +30,42 @@ public class RoomRestControllerTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    /**
+     * Validate room data and the response message.
+     * @throws Exception
+     */
     @Test
     public void testGetAllRooms() throws Exception {
-        when(roomService.getAllRooms()).thenReturn(null);
+
+        when(roomService.getAllRooms()).thenReturn(Optional.of(Arrays.asList(prepareTestDate())));
 
         ResponseEntity<SsaSimpleResponse> result = roomRestController.getAllRooms();
-        Assert.assertEquals(null, result);
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals("Rooms has been fetched", result.getBody().getMessage());
+    }
+
+    /**
+     * Validate room data and the negative response message.
+     * @throws Exception
+     */
+    @Test
+    public void testGetAllRoomsFailure() throws Exception {
+
+        when(roomService.getAllRooms()).thenReturn(Optional.empty());
+
+        ResponseEntity<SsaSimpleResponse> result = roomRestController.getAllRooms();
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals("Unable to fetch rooms.", result.getBody().getMessage());
+    }
+
+    /**
+     * prepare test data for room controller
+     * @return room instance
+     */
+    private Room prepareTestDate(){
+        Room room = new Room();
+        room.setId(1);
+        room.setName("Ward 1");
+        return room;
     }
 }
