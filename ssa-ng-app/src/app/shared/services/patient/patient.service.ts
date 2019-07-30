825072463904342889b479fa8f0de12d8d25d819
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-31 03:21:49 +0530

    added util service & method to get patients

--- ssa-ng-app/src/app/shared/services/patient/patient.service.ts
+++ ssa-ng-app/src/app/shared/services/patient/patient.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Patient } from 'src/app/shared/intefaces/patient';
+import { Patient } from 'src/app/shared/interfaces/patient/patient';
+import { UtilService } from '../util/util.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
+  endpoint: string = UtilService.getServerUrl().concat('patient');
   constructor(private httpCliet: HttpClient) { }
 
   public addPatient(patient: Patient) {
-      return this.httpCliet.post('http://localhost:8080/ssa/api/v1/patient', patient);
+    try {
+      return this.httpCliet.post(this.endpoint, patient);
+    } catch (e) {
+      return;
+    }
+  }
+
+  public getAllPatient() {
+    try {
+      return this.httpCliet.get(this.endpoint);
+    } catch (e) {
+      return;
+    }
   }
 
 }
