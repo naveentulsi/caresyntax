7cac163493dd77c2626274617972d33056a2c475
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-07-31 03:20:30 +0530

    added procedure constants

--- src/main/java/com/caresyntax/ssa/utility/IConstants.java
+++ src/main/java/com/caresyntax/ssa/utility/IConstants.java
@@ -1,16 +1,23 @@
 package com.caresyntax.ssa.utility;
 
 public interface IConstants {
-    String NO_PATIENT_FETCHED = "No patient data";
-    String NO_DOCTOR_FETCHED = "No doctors has been fetched";
+    String PATIENT_NOT_FETCHED = "No patient data";
+    String DOCTOR_NOT_FETCHED = "No doctors has been fetched";
     String DOCTOR_FETCHED = "Successfully fetched doctors";
     String PATIENT_FETCHED = "Successfully fetched patients";
     String UNABLE_ADD_PATIENT = "Server unable to add patient";
     String PATIENT_ADDED = "Patient added successfully";
     String DEFAULT_SCHEMA = "caresyntax";
     String DATE_FORMAT = "yyyy/MM/dd";
+    String PROCEDURE_NOT_UPDATED = "Server unable to update procedures";
     String PROCEDURE_EMPTY_STATUS = "Server unable to update procedures, cannot update empty status";
     String PROCEDURE_INVALID_ID = "Server unable to update procedures, unable to find procedure with provided Id";
     String PROCEDURE_NO_ID = "Server unable to update procedures, since procedure Id is empty";
     String PROCEDURE_UPDATED = "Status updated successfully";
+    String PROCEDURE_FETCH_FAILED = "Unable to fetch procedures";
+    String PROCEDURE_FETCH_SUCCESS = "Successfully fetched procedures";
+    String PROCEDURE_NOT_SAVE = "Server unable to store procedures";
+    String DOCTOR_NOT_SAVED = "Unable to save doctor now.";
+    String PROCEDURE_SAVED = "Procedure has been saved successfully";
+
 }
